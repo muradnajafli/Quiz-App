f9cccb6f894bc80526b3cd18b5b834fd007cfce8
Akın  Yıldırım <aknyldrm361@gmail.com>
2024-02-24 14:23:56 +0300

    unnecessary functions removed

--- app/src/main/java/com/muradnajafli/quizapp/data/QuizDao.kt
+++ app/src/main/java/com/muradnajafli/quizapp/data/QuizDao.kt
@@ -9,18 +9,9 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface QuizDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(question: Question)
-
-    @Delete
-    suspend fun delete(question: Question)
-
     @Query("SELECT * FROM questions")
     fun getAllQuestions(): Flow<List<Question>>
 
     @Query("SELECT * FROM questions WHERE questionCategory = :category")
     fun getQuestionsByCategory(category: QuestionCategory): Flow<List<Question>>
-
-    @Query("SELECT * FROM questions WHERE id = :id")
-    suspend fun getQuestionById(id: Int): Question?
 }
--- app/src/main/java/com/muradnajafli/quizapp/data/QuizRepository.kt
+++ app/src/main/java/com/muradnajafli/quizapp/data/QuizRepository.kt
@@ -4,14 +4,6 @@ import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 interface QuizRepository {
-
-    suspend fun insert(question: Question)
-
-    suspend fun delete(question: Question)
-
     fun getAllQuestions(): Flow<List<Question>>
-
     fun getQuestionsByCategory(category: QuestionCategory): Flow<List<Question>>
-
-    suspend fun getQuestionById(id: Int): Question?
 }
